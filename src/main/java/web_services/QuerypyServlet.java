a4089ea51c07a545116c2708fcc389cb35dab678
TongXiqing <418525836@qq.com>
2015-04-02 12:33:51 +0800

    new
    new

---
+++ src/main/java/web_services/QuerypyServlet.java
@@ -0,0 +1,27 @@
+package web_services;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by tongxiqing on 2015/4/2.
+ */
+public class QuerypyServlet extends HttpServlet {
+
+    public void init() throws ServletException {
+
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getAttribute("accountStb");
+        req.getAttribute("productCode");
+    }
+
+}
---
+++ src/main/java/web_services/UpdatepyServlet.java
@@ -0,0 +1,9 @@
+package web_services;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by tongxiqing on 2015/4/2.
+ */
+public class UpdatepyServlet extends HttpServlet {
+}
